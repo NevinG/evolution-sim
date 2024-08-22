ee28d9acbc0a743bacf51087ef9243efc6b2604f
Nevin Gilday <nevingilday@gmail.com>
2024-08-22 15:10:53 -0500

    added basic visualization of agents

--- src/main.rs
+++ src/main.rs
@@ -51,6 +51,7 @@ impl BaseNode for Node {
         //add bias to output
         output += self.bias;
 
+        //TODO: research what activation functions I should use for this project
         //apply activation function on output
         output = output.tanh();
         
@@ -285,16 +286,127 @@ impl World {
         }
     }
 }
+
+use std::num::NonZeroU32;
+
+use femtovg::renderer::OpenGl;
+use femtovg::{Canvas, Color, Renderer};
+use glutin::surface::Surface;
+use glutin::{context::PossiblyCurrentContext, display::Display};
+use glutin_winit::DisplayBuilder;
+use raw_window_handle::HasRawWindowHandle;
+use winit::dpi::PhysicalPosition;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use winit::{dpi::PhysicalSize, window::Window};
+use std::time::{Duration, Instant};
+
+use glutin::{
+    config::ConfigTemplateBuilder,
+    context::ContextAttributesBuilder,
+    display::GetGlDisplay,
+    prelude::*,
+    surface::{SurfaceAttributesBuilder, WindowSurface},
+};
 fn main() {
     let mut world = World::new();
-    world.add_n_agents(10);
+    world.add_n_agents(100);
 
-    for i in 0..100 {
+    let event_loop = EventLoop::new();
+    let (context, gl_display, window, surface) = create_window(&event_loop);
+
+    let renderer = unsafe { OpenGl::new_from_function_cstr(|s| gl_display.get_proc_address(s) as *const _) }
+        .expect("Cannot create renderer");
+
+    let mut canvas = Canvas::new(renderer).expect("Cannot create canvas");
+    canvas.set_size(1000, 600, window.scale_factor() as f32);
+
+    const FRAME_RATE: u32 = 30;
+    let frame_duration = Duration::from_secs(1) / FRAME_RATE;
+    let mut last_frame_time = Instant::now();
+    event_loop.run(move |event, _target, control_flow| {
         world.simulate_frame();
 
-        println!("------------FRAME {}------------", i);
-        for (i, agent) in world.agents.iter().enumerate() {
-            println!("Agent {i}: x: {}, y: {}", agent.borrow().x, agent.borrow().y);
+        if last_frame_time.elapsed() >= frame_duration {
+            render(&context, &surface, &window, &mut canvas, &world);
+            last_frame_time = Instant::now();
         }
+
+        //close window on exit
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                _ => {}
+            }
+            _ => {}
+    }})
+}
+
+fn create_window(event_loop: &EventLoop<()>) -> (PossiblyCurrentContext, Display, Window, Surface<WindowSurface>) {
+    let window_builder = WindowBuilder::new()
+        .with_inner_size(PhysicalSize::new(1000., 600.))
+        .with_title("Femtovg");
+
+    let template = ConfigTemplateBuilder::new().with_alpha_size(8);
+
+    let display_builder = DisplayBuilder::new().with_window_builder(Some(window_builder));
+
+    let (window, gl_config) = display_builder
+        .build(event_loop, template, |mut configs| configs.next().unwrap())
+        .unwrap();
+
+    let window = window.unwrap();
+
+    let gl_display = gl_config.display();
+
+    let context_attributes = ContextAttributesBuilder::new().build(Some(window.raw_window_handle()));
+
+    let mut not_current_gl_context =
+        Some(unsafe { gl_display.create_context(&gl_config, &context_attributes).unwrap() });
+
+    let attrs = SurfaceAttributesBuilder::<WindowSurface>::new().build(
+        window.raw_window_handle(),
+        NonZeroU32::new(1000).unwrap(),
+        NonZeroU32::new(600).unwrap(),
+    );
+
+    let surface = unsafe { gl_config.display().create_window_surface(&gl_config, &attrs).unwrap() };
+
+    (
+        not_current_gl_context.take().unwrap().make_current(&surface).unwrap(),
+        gl_display,
+        window,
+        surface,
+    )
+}
+
+fn render<T: Renderer>(
+    context: &PossiblyCurrentContext,
+    surface: &Surface<WindowSurface>,
+    window: &Window,
+    canvas: &mut Canvas<T>,
+    world : &World,
+) {
+    // Make sure the canvas has the right size:
+    let size = window.inner_size();
+    canvas.set_size(size.width, size.height, window.scale_factor() as f32);
+
+    canvas.clear_rect(0, 0, size.width, size.height, Color::black());
+
+    // Make smol red rectangle
+    for agent in &world.agents {
+        let square_position = PhysicalPosition::new(agent.borrow().x as f64, agent.borrow().y as f64);
+        canvas.clear_rect(
+            square_position.x as u32,
+            square_position.y as u32,
+            15,
+            15,
+            Color::rgbf(1., 0., 0.),
+        );
     }
+    // Tell renderer to execute all drawing commands
+    canvas.flush();
+    // Display what we've just rendered
+    surface.swap_buffers(context).expect("Could not swap buffers");
 }
